6764f4b960171b53324b6e192666e197e2c91061
miroslavnanovski <156704544+miroslavnanovski@users.noreply.github.com>
2025-03-06 03:24:50 +0200

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Button from '@mui/material/Button';
+
+function App() {
+
+
+  return (
+    <>
+    <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Hello!</button>
+    </>
+  )
+}
+
+export default App;
